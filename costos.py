bdf514e38d55c2aafd76c232b9dfbe80f0337e19
unknown <nicolasescobar0325@gmail.com>
2018-01-25 11:39:50 -0500

    First commit

---
+++ costos.py
@@ -0,0 +1,8 @@
+val_ini = 14988032  # 3985
+precio_sur = 21803945
+fee_compra = 0.008
+solve = (val_ini / precio_sur) * (1 - fee_compra)
+envio_cex = 0.0046
+solve = solve - envio_cex
+#ajuste = solve - 0.00005854
+print(5020000 / 19345000)
